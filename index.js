f47021c4b13bfadf9d9a4b57e79095877fadce09
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-10-07 10:48:01 +0200

    Add tab style

--- index.js
+++ index.js
@@ -1,75 +1,209 @@
-'use strict';
+'use strict'
 
-var util = require('./util')
-var hexToRgb = util.hexToRgb
-var hexToRgba = util.hexToRgba
-var lighten = util.lighten
+const {lighten} = require('./util')
 
-const black   = '#2d3e4f';
-const red     = '#be3a31';
-const green   = '#30ad63';
-const yellow  = '#f19b2c';
-const blue    = '#3498db';
-const magenta = '#BB4698';
-const cyan    = '#239f85';
-const gray    = '#d9dfdd';
+/**
+ * Colors
+ */
 
-const lightBlack   = lighten(black, 25)
-const lightRed     = lighten(red, 25)
-const lightGreen   = lighten(green, 25)
-const lightYellow  = lighten(yellow, 25)
-const lightBlue    = lighten(blue, 25)
+const black = '#2d3e4f'
+const red = '#be3a31'
+const green = '#30ad63'
+const yellow = '#f19b2c'
+const blue = '#3498db'
+const magenta = '#BB4698'
+const cyan = '#239f85'
+const gray = '#d9dfdd'
+
+const lightBlack = lighten(black, 25)
+const lightRed = lighten(red, 25)
+const lightGreen = lighten(green, 25)
+const lightYellow = lighten(yellow, 25)
+const lightBlue = lighten(blue, 25)
 const lightMagenta = lighten(magenta, 25)
-const lightCyan    = lighten(cyan, 25)
-const lightGray    = lighten(gray, 25)
+const lightCyan = lighten(cyan, 25)
+const lightGray = lighten(gray, 25)
 
-const backgroundColor = '#000';
-const borderColor = backgroundColor;
-const activeColor = '#80CBC4'
+/**
+ * Variables
+ */
 
-const foregroundColor = '#dadfdd';
-const cursorColor = lighten(magenta, 12.5);
+const primaryColor = '#80CBC4'
+const secondaryColor = '#d4a14b'
+
+/**
+ * General
+ */
+
+const backgroundColor = '#000'
+const foregroundColor = '#dadfdd'
+const borderColor = backgroundColor
+const cursorColor = lightMagenta
+const selectionColor = secondaryColor
+const linkColor = secondaryColor
+
+/**
+ * Tabs
+ */
+
+const inactiveTabBackgroundColor = '#171717'
+const activeTabBorderColor = primaryColor
+const activeTabColor = primaryColor
+const dividerColor = 'rgba(157, 165, 180, 0.25)'
 
 exports.decorateConfig = config => {
-	return Object.assign({}, config, {
-		backgroundColor,
-		foregroundColor,
-		borderColor,
-		cursorColor,
-		colors: [
-			black,
-			red,
-			green,
-			yellow,
-			blue,
-			magenta,
-			cyan,
-			gray,
+  return Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    borderColor,
+    cursorColor,
+    colors: [
+      black,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      gray,
 
 			// light
-			lightBlack,
-			lightRed,
-			lightGreen,
-			lightYellow,
-			lightBlue,
-			lightMagenta,
-			lightCyan,
-			lightGray
-		],
-		css: `
+      lightBlack,
+      lightRed,
+      lightGreen,
+      lightYellow,
+      lightBlue,
+      lightMagenta,
+      lightCyan,
+      lightGray
+    ],
+    css: `
 			${config.css || ''}
-			.tab_active:before {
-				border-color: ${hexToRgba(activeColor, 0.8)};
+			.splitpane_divider {
+				background-color: ${dividerColor} !important;
 			}
+			.header_header {
+        top: 0;
+        right: 0;
+        left: 0;
+      }
+      .tabs_list {
+        background-color: ${inactiveTabBackgroundColor} !important;
+      }
+      .tab_tab {
+        font-weight: 500;
+        color: ${dividerColor};
+        border-width: 0 0 0 1px;
+        border-image: linear-gradient(#21252b, #181a1f 1em) 0 0 0 1 stretch;
+        border-style: solid;
+      }
+      .tab_tab:first-of-type {
+        border-width: 0;
+      }
+      .tab_tab:hover {
+        color: ${dividerColor};
+        transition: none;
+      }
+      .tab_tab::after {
+        content: "";
+        position: absolute;
+        pointer-events: none;
+        top: 0;
+        bottom: -1px;
+        left: 0;
+        width: 2px;
+        height: inherit;
+        background: ${activeTabBorderColor};
+        opacity: 0;
+        transition: opacity .16s;
+        z-index: 1;
+      }
+      .tabs_title,
+      .tab_tab.tab_active {
+        font-weight: 500;
+        color: ${activeTabColor};
+      }
+      .tab_tab.tab_active {
+        background-color: ${backgroundColor};
+      }
+      .tab_tab.tab_active,
+      .tab_tab.tab_active + .tab_tab {
+        border-image: linear-gradient(transparent, transparent) 0 0 0 1 stretch;
+      }
+      .tab_tab.tab_active::before {
+        content: "";
+        z-index: 1;
+        position: absolute;
+        top: 0;
+        left: -1px;
+        bottom: -1px;
+        right: 0;
+        height: inherit;
+        background-image: linear-gradient(rgba(255, 255, 255, 0.02), rgba(255, 255, 255, 0));
+        box-shadow: inset 0 1px 1px rgba(255, 255, 255, 0.06);
+        border: 1px solid #181a1f;
+        border-bottom-color: ${backgroundColor};
+        border-top: 0;
+      }
+      .tab_tab.tab_active:last-of-type::before {
+        border-right-width: 0;
+      }
+      .tab_tab.tab_active::after {
+        opacity: 1;
+        transition-duration: .32s;
+      }
+      .tab_icon {
+        display: block;
+        background: rgba(157, 165, 180, 0.6);
+        -webkit-mask-image: url('${__dirname}/close.svg');
+        mask-image: url('${__dirname}/close.svg');
+        -webkit-mask-size: 7px;
+        mask-size: 7px;
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-position-y: center;
+        mask-position-y: center;
+        -webkit-mask-position-x: 8px;
+        mask-position-x: 8px;
+        width: 26px;
+        height: 100%;
+        top: 0;
+        right: 0;
+        transform: scale(0);
+        transition: transform .08s;
+        opacity: 1;
+        border-radius: 0;
+        z-index: 2;
+      }
+      .tab_icon:hover {
+        background: rgba(157, 165, 180, 0.6);
+        opacity: .7;
+      }
+      .tab_tab.tab_active .tab_icon {
+        background: #d7dae0;
+      }
+      .tab_tab.tab_active .tab_icon:hover {
+        background: #d7dae0;
+      }
+      .tab_tab:hover .tab_icon {
+        transform: scale(1);
+        transition-duration: .16s;
+      }
+      .tab_icon svg {
+        display: none;
+      }
+      .tab_icon::after {
+        display: none;
+      }
 		`,
-		termCSS: `
-		  x-screen a {
-		    color: ${activeColor}
+    termCSS: `
+			${config.termCSS || ''}
+			x-screen a {
+		    color: ${linkColor};
 		  }
-
 			::selection {
-			  background: ${activeColor}
+			  background: ${selectionColor};
 			}
 		`
-	});
-};
+  })
+}
--- util.js
+++ util.js
@@ -1,59 +1,34 @@
-'use strict';
-
-/**
- * https://github.com/supercrabtree/tie-dye/blob/master/hexToRgb.js
- */
-function hexToRgb(hex) {
-  var match = hex.toString(16).match(/[a-f0-9]{6}/i);
-	var r = 0;
-	var g = 0;
-	var b = 0;
-
-  if (!match) return { r, g, b }
-
-  var integer = parseInt(match[0], 16);
-
-  var r = (integer >> 16) & 0xFF;
-  var g = (integer >> 8) & 0xFF;
-  var b = integer & 0xFF;
-
-  return { r, g, b }
-}
-
-function hexToRgba(hex, alpha) {
-	var rgb = hexToRgb(hex)
-	return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${alpha})`
-}
+'use strict'
 
 /**
  * https://css-tricks.com/snippets/javascript/lighten-darken-color/
  */
-function lighten(col, amt) {
-    var usePound = false;
+function lighten (col, amt) {
+  var usePound = false
 
-    if (col[0] == "#") {
-      col = col.slice(1);
-      usePound = true;
-    }
+  if (col[0] == '#') {
+    col = col.slice(1)
+    usePound = true
+  }
 
-    var num = parseInt(col,16);
+  var num = parseInt(col, 16)
 
-    var r = (num >> 16) + amt;
+  var r = (num >> 16) + amt
 
-    if (r > 255) r = 255;
-    else if  (r < 0) r = 0;
+  if (r > 255) r = 255
+  else if (r < 0) r = 0
 
-    var b = ((num >> 8) & 0x00FF) + amt;
+  var b = ((num >> 8) & 0x00FF) + amt
 
-    if (b > 255) b = 255;
-    else if  (b < 0) b = 0;
+  if (b > 255) b = 255
+  else if (b < 0) b = 0
 
-    var g = (num & 0x0000FF) + amt;
+  var g = (num & 0x0000FF) + amt
 
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
+  if (g > 255) g = 255
+  else if (g < 0) g = 0
 
-    return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16)
 }
 
-module.exports = {hexToRgb, hexToRgba, lighten}
+module.exports = {lighten}
